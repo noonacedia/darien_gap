21ceb1a6e89b52768d7c8346523a448943148dbe
noonacedia <ruyearn@gmail.com>
2025-02-05 18:08:42 +0400

    [develop] Add uvicorn dev server starter and mock Litestar app

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,10 @@
-from uvicorn import uvicorn
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.app.main:app",
+        port=8000,
+        log_level="info",
+        reload=True,
+    )
